dec414783dc0a9eb039f855505dab3d0310b867e
Neerthigan Sivananthan <nsivananthan@gobasports.com>
2017-10-04 21:35:13 -0400

    Added free form crop feature

--- scripts/custom/draggable.js
+++ scripts/custom/draggable.js
@@ -3,17 +3,65 @@ window.onload = function() {
 };
 
 function init () {
-    //Retreive the canvas element
-    var stage = new createjs.Stage("demoCanvas");
+    var c = createjs, stage, art, testImg;
+    var x, y, listener, color, hue=0;
 
-    //Wrap the uploaded image
-    var bitmap = new createjs.Bitmap("./images/blue_oxford_520_520.jpg");
+    stage = new c.Stage('cropCanvas');
 
-    //When the image is loaded, add the image to canvas and update it.
-    bitmap.image.onload = function() {
-        console.log(bitmap);
-        stage.addChild(bitmap);
+    testImg = new c.Bitmap('./images/blue_oxford_520_520.jpg');
+
+    var cont = stage.addChild(new c.Container());
+
+    testImg.image.onload = function() {
+        cont.updateCache("source-over");
+        cont.removeChild(testImg);
         stage.update();
     };
+
+    art = new c.Shape()
+    cont.cache(0,0,600,400);
+    cont.addChild(testImg, art);
+
+    stage.on("stagemousedown", startDraw, this);
+
+    function startDraw(evt) {
+        listener = stage.on("stagemousemove", draw, this);
+        stage.on("stagemouseup", endDraw, this);
+        color = c.Graphics.getHSL(hue+=85, 50, 50);
+        x = evt.stageX-0.001; // offset so we draw an initial dot
+        y = evt.stageY-0.001;
+        draw(evt); // draw the initial dot
+    }
+
+    function draw(evt) {
+        art.graphics.ss(20,1).s(color).mt(x,y).lt(evt.stageX, evt.stageY);
+
+        // the composite operation is the secret sauce.
+        // we'll either draw or erase what the user drew.
+        cont.updateCache(erase.checked ? "destination-out" : "source-over");
+
+        art.graphics.clear();
+        x = evt.stageX;
+        y = evt.stageY;
+        stage.update();
+    }
+
+    function endDraw(evt) {
+        stage.off("stagemousemove", listener);
+        evt.remove();
+    }
+
+    //Retreive the canvas element
+    // var stage = new createjs.Stage("demoCanvas");
+    //Wrap the uploaded image
+    // var bitmap = new createjs.Bitmap("./images/blue_oxford_520_520.jpg");
+    //When the image is loaded, add the image to canvas and update it.
+    // bitmap.image.onload = function() {
+    //     console.log(bitmap);
+    //     stage.addChild(bitmap);
+    //     stage.update();
+    // };
+
+
 
 }
