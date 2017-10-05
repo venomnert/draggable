b7f66e5a70866ec6cd8bf0151ed9cb624a078c65
layanas <jlayanas@gmail.com>
2017-10-04 21:30:09 -0400

    merged upload demo

--- scripts/custom/draggable.js
+++ scripts/custom/draggable.js
@@ -17,3 +17,21 @@ function init () {
     };
 
 }
+
+
+
+
+function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#blah')
+                        .attr('src', e.target.result)
+                        .width(350)
+                        .height(350);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
